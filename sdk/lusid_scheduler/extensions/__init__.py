f760af761ab0cb0eeaafd99fbf2773eb9dc079fc
Concourse <concourse@finbourne.com>
2023-11-30 09:33:06 +0000

    Updates for v2.0.1-beta

--- sdk/lusid_scheduler/extensions/__init__.py
+++ sdk/lusid_scheduler/extensions/__init__.py
@@ -1,15 +1,7 @@
-from lusid_scheduler.extensions.api_client_builder import build_client
 from lusid_scheduler.extensions.api_client_factory import SyncApiClientFactory, ApiClientFactory
 from lusid_scheduler.extensions.configuration_loaders import (
     ConfigurationLoader,
     SecretsFileConfigurationLoader,
     EnvironmentVariablesConfigurationLoader,
     ArgsConfigurationLoader,
-    get_api_configuration,
 )
-from lusid_scheduler.extensions.api_configuration import ApiConfiguration
-from lusid_scheduler.extensions.retry import RetryingRestWrapper, RetryingRestWrapperAsync
-from lusid_scheduler.extensions.proxy_config import ProxyConfig
-from lusid_scheduler.extensions.refreshing_token import RefreshingToken
-from lusid_scheduler.extensions.api_client import SyncApiClient
-from lusid_scheduler.extensions.rest import RESTClientObject
